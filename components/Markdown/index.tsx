6221a5a74a290f218c591a53e45369744ca3ee6e
Len E-Herng <leneherng@gmail.com>
2023-10-06 17:48:12 +0800

    chore: add remark-gfm for table support

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from "react";
+import remarkGfm from 'remark-gfm'
 import ReactMarkdown, { Options } from "react-markdown";
 
 interface MarkdownProps extends Options {}
 
 const Markdown: FunctionComponent<MarkdownProps> = ({ className = "markdown", children }) => {
-  return <ReactMarkdown className={className}>{children}</ReactMarkdown>;
+  return <ReactMarkdown remarkPlugins={[remarkGfm]} className={className}>{children}</ReactMarkdown>;
 };
 
 export default Markdown;
