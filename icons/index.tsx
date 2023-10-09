66b8a5ef6124acf68d6d8e572e62052f55791d52
Len E-Herng <leneherng@gmail.com>
2023-10-09 18:06:54 +0800

    chore: add Folder icon

--- icons/index.tsx
+++ icons/index.tsx
@@ -163,7 +163,7 @@ export const ParlimenIcon = (props: ComponentProps<"svg">) => {
       />
     </svg>
   );
-}
+};
 
 export const MenuIcon = (props: ComponentProps<"svg">) => {
   return (
@@ -199,4 +199,39 @@ export const MenuIcon = (props: ComponentProps<"svg">) => {
       </g>
     </svg>
   );
-}
+};
+
+export const FolderIcon = (props: ComponentProps<"svg">) => {
+  return (
+    <svg
+      width="84"
+      height="64"
+      viewBox="0 0 84 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M0 8V29.4603H62.747L35.7426 2.35382C34.2414 0.847002 32.202 0 30.075 0H8C3.58172 0 0 3.58172 0 8Z"
+        fill="#94A3B8"
+      />
+      <path
+        d="M31.0871 11.1746H8C3.58172 11.1746 0 14.7563 0 19.1746V56C0 60.4183 3.58172 64 8 64H76C80.4183 64 84 60.4183 84 56V12.0635C84 7.6452 80.4183 4.06348 76 4.06348H44.8165C42.6896 4.06348 40.6502 4.91048 39.149 6.4173L36.7546 8.82076C35.2535 10.3276 33.2141 11.1746 31.0871 11.1746Z"
+        fill="url(#linear_gradient)"
+      />
+      <defs>
+        <linearGradient
+          id="linear_gradient"
+          x1="42"
+          y1="50"
+          x2="42"
+          y2="4"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.494792" stop-color="#D6E0EC" />
+          <stop offset="1" stop-color="#F0F6FE" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
