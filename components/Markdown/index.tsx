b336c064db246d6cfeb6332c7e16b16a2e9be7dc
Len E-Herng <leneherng@gmail.com>
2023-10-18 18:37:15 +0800

    chore: add props

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,11 +1,19 @@
 import { FunctionComponent } from "react";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 import ReactMarkdown, { Options } from "react-markdown";
 
 interface MarkdownProps extends Options {}
 
-const Markdown: FunctionComponent<MarkdownProps> = ({ className = "markdown", children }) => {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]} className={className}>{children}</ReactMarkdown>;
+const Markdown: FunctionComponent<MarkdownProps> = ({
+  className = "markdown",
+  children,
+  ...props
+}) => {
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]} className={className} {...props}>
+      {children}
+    </ReactMarkdown>
+  );
 };
 
 export default Markdown;
