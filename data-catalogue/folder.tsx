55517bf1ad18e6b8af1c0e27e1c0836bf9183116
Len E-Herng <leneherng@gmail.com>
2023-10-18 18:44:54 +0800

    fix: anchor, push path, add tracking, download

--- data-catalogue/folder.tsx
+++ data-catalogue/folder.tsx
@@ -8,11 +8,17 @@ import {
 } from "@components/Dialog";
 import { DateCard, Dropdown } from "@components/index";
 import { BookmarkIcon } from "@heroicons/react/24/solid";
+import { useAnalytics } from "@hooks/useAnalytics";
 import { useTranslation } from "@hooks/useTranslation";
 import { ClosedFolderIcon, OpenFolderIcon } from "@icons/index";
-import { cn } from "@lib/helpers";
+import { get } from "@lib/api";
+import { BREAKPOINTS } from "@lib/constants";
+import { WindowContext } from "@lib/contexts/window";
+import { cn, download } from "@lib/helpers";
+import { routes } from "@lib/routes";
 import { Sitting } from "@lib/types";
-import { useState } from "react";
+import Link from "next/link";
+import { useContext, useState } from "react";
 
 /**
  * Catalogue Folder
@@ -21,24 +27,38 @@ import { useState } from "react";
 
 interface CatalogueFolderProps {
   dateRange: string;
+  onClick: () => void;
+  onClose: () => void;
   meeting: string;
   sitting_list: Sitting[];
 }
 
 export default function CatalogueFolder({
   dateRange,
+  onClick,
+  onClose,
   meeting,
   sitting_list,
 }: CatalogueFolderProps) {
   const { t, i18n } = useTranslation(["catalogue", "enum"]);
   const [open, setOpen] = useState<boolean>(false);
+  const { size } = useContext(WindowContext);
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        setOpen(open);
+        if (!open) onClose();
+      }}
+    >
       <DialogTrigger>
         <div
           className="flex flex-col gap-y-3 items-center pt-2"
-          onClick={() => setOpen(!open)}
+          onClick={() => {
+            setOpen(!open);
+            onClick();
+          }}
         >
           <div className="relative">
             <div
@@ -84,46 +104,87 @@ export default function CatalogueFolder({
           <DialogClose></DialogClose>
         </DialogHeading>
         <DialogDescription>
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 max-h-[80vh] overflow-y-auto">
-            {sitting_list.map((sitting) => (
-              <div className="flex items-center gap-x-4.5 p-3 shadow-button hover:shadow-floating border dark:border-zinc-800 rounded-lg bg-white dark:bg-zinc-900">
-                <DateCard date={sitting.date} size="sm" />
-                <div className="flex flex-col gap-y-1.5">
-                  <p className="font-medium">
-                    {new Date(sitting.date).toLocaleDateString(i18n.language, {
-                      weekday: "long",
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                    })}
-                  </p>
-                  <div>
-                    <p className="text-blue-600 dark:text-primary-dark flex gap-1.5 text-sm items-center whitespace-nowrap flex-wrap">
-                      <span className="dark:hover:text-blue-600">
-                        {t("cite")}
-                      </span>
-                      •
-                      <Dropdown
-                        className="p-0 border-none shadow-none text-blue-600 dark:text-primary-dark font-normal gap-1 dark:hover:bg-transparent dark:active:bg-transparent dark:hover:text-blue-600"
-                        width="w-fit"
-                        placeholder={t("download")}
-                        selected={undefined}
-                        options={[
-                          { label: "pdf", value: "pdf" },
-                          { label: "csv", value: "csv" },
-                        ]}
-                        // FIXME: download onChange
-                        onChange={(e) => e}
-                      />
-                      •
-                      <span className="dark:hover:text-blue-600">
-                        {t("share")}
-                      </span>
+          <div
+            className={cn(
+              "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 max-h-[80vh]",
+              sitting_list.length < 4
+                ? "overflow-visible"
+                : "overflow-y-auto scroll"
+            )}
+          >
+            {sitting_list.map((sitting, i) => {
+              const { track } = useAnalytics(sitting.filename);
+
+              const cols = size.width < BREAKPOINTS.XL ? 2 : 3;
+              const modulo = sitting_list.length % cols;
+              const itemsInLastRow =
+                size.width < BREAKPOINTS.MD ? 1 : modulo === 0 ? cols : modulo;
+
+              return (
+                <Link
+                  href={`${
+                    sitting.filename.startsWith("dr")
+                      ? routes.HANSARD_DR
+                      : routes.HANSARD_DN
+                  }/${sitting.date}`}
+                  prefetch={false}
+                  className="flex items-center gap-x-4.5 p-3 shadow-button hover:shadow-floating border dark:border-zinc-800 rounded-lg bg-white dark:bg-zinc-900"
+                >
+                  <DateCard date={sitting.date} size="sm" />
+                  <div className="flex flex-col gap-y-1.5">
+                    <p className="font-medium">
+                      {new Date(sitting.date).toLocaleDateString(
+                        i18n.language,
+                        {
+                          weekday: "long",
+                          year: "numeric",
+                          month: "short",
+                          day: "numeric",
+                        }
+                      )}
                     </p>
+                    <div>
+                      <p className="text-blue-600 dark:text-primary-dark flex gap-1.5 text-sm items-center whitespace-nowrap flex-wrap">
+                        <span className="dark:hover:text-blue-600 hover:underline [text-underline-position:from-font]">
+                          {t("cite")}
+                        </span>
+                        •
+                        <Dropdown
+                          className="p-0 border-none shadow-none text-blue-600 dark:text-primary-dark hover:underline [text-underline-position:from-font] font-normal gap-1 dark:hover:bg-transparent active:bg-transparent dark:active:bg-transparent dark:hover:text-blue-600"
+                          width="w-fit"
+                          placeholder={t("download")}
+                          selected={undefined}
+                          options={[
+                            { label: "PDF", value: "pdf" },
+                            { label: t("csv.title"), value: "csv" },
+                          ]}
+                          anchor={
+                            i > sitting_list.length - itemsInLastRow - 1
+                              ? "bottom-6"
+                              : ""
+                          }
+                          onChange={({ value: filetype }) => {
+                            download(
+                              `${process.env.NEXT_PUBLIC_DOWNLOAD_URL}${
+                                sitting.filename.startsWith("dr")
+                                  ? "dewanrakyat"
+                                  : "dewannegara"
+                              }/${sitting.filename}.${filetype}`,
+                              `sitting.filename.${filetype}`
+                            );
+                            track(filetype);
+                          }}
+                        />
+                        •
+                        <span className="dark:hover:text-blue-600 hover:underline [text-underline-position:from-font]">
+                          {t("share")}
+                        </span>
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                </Link>
+              );
+            })}
           </div>
         </DialogDescription>
       </DialogContent>
