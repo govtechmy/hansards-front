dbdbef1c9dfb690a58e91f1af62ac5ffe9b12e6f
Len E-Herng <leneherng@gmail.com>
2023-11-06 18:19:47 +0800

    wip: share button

--- data-catalogue/share.tsx
+++ data-catalogue/share.tsx
@@ -1,4 +1,5 @@
 import At from "@components/At";
+import Button from "@components/Button";
 import {
   Dialog,
   DialogContent,
@@ -25,7 +26,7 @@ interface ShareButtonProps {
 }
 
 export default function ShareButton({ id, trigger }: ShareButtonProps) {
-  const { t, i18n } = useTranslation(["hansard", "catalogue"]);
+  const { t } = useTranslation(["hansard", "catalogue"]);
   const [open, setOpen] = useState<boolean>(false);
   const [copyText, setCopyText] = useState<string>("copy");
 
@@ -40,10 +41,9 @@ export default function ShareButton({ id, trigger }: ShareButtonProps) {
           text: "Hansard Parlimen",
           url: `https://hansard.parlimen.gov.my/hansard/${id}`,
         })
-        .then(() => console.log("Successful share"))
         .catch((error) => console.log("Error sharing", error));
     } else setOpen(!open);
-  }
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -51,16 +51,13 @@ export default function ShareButton({ id, trigger }: ShareButtonProps) {
         {trigger ? (
           trigger(onClick)
         ) : (
-          <button
-            className="bt"
-            onClick={onClick}
-          >
+          <div className="bt" onClick={onClick}>
             <div className={"shr"} />
             {t("share")}
-          </button>
+          </div>
         )}
       </DialogTrigger>
-      <DialogContent className="w-full border border-slate-200 dark:border-zinc-800 bg-slate-50 dark:bg-zinc-900 p-6 flex flex-col gap-y-5">
+      <DialogContent className="w-fit border border-slate-200 dark:border-zinc-800 bg-slate-50 dark:bg-zinc-900 p-6 flex flex-col gap-y-5">
         <DialogHeading>
           <span className="font-medium text-zinc-900 dark:text-white">
             {t("share")}
@@ -72,49 +69,49 @@ export default function ShareButton({ id, trigger }: ShareButtonProps) {
             {[
               {
                 name: "Twitter",
-                icon: <XShare className="h-8 w-8" />,
+                icon: <XShare className="h-4 w-4" />,
                 link: `https://www.twitter.com/intent/tweet?text=${title}&url=${URL}&hashtags=hansard`,
               },
               {
                 name: "Facebook",
-                icon: <FBShare className="h-8 w-8" />,
+                icon: <FBShare className="h-5 w-5" />,
                 link: `https://www.facebook.com/sharer/sharer.php?u=${URL}&t=${title}`,
               },
               {
                 name: "E-mail",
-                icon: <EnvelopeIcon className="h-8 w-8" />,
+                icon: <EnvelopeIcon className="h-5 w-5" />,
                 link: `mailto:?subject=${title}&body=${URL}`,
               },
               {
                 name: t(copyText, { ns: "common" }),
-                icon: <DocumentDuplicateIcon className="h-8 w-8" />,
+                icon: <DocumentDuplicateIcon className="h-5 w-5" />,
                 link: "copy",
               },
             ].map(({ name, icon, link }) => {
               const { result, realtime_track } = useContext(AnalyticsContext);
 
               return (
-                <div className="flex flex-col gap-3 justify-center items-center h-24 p-3 shadow-button border dark:border-zinc-800 rounded-lg bg-white dark:bg-zinc-900">
+                // <div className="flex flex-col gap-3 justify-center items-center h-24 p-3 shadow-button border dark:border-zinc-800 rounded-lg bg-white dark:bg-zinc-900">
+                <Button
+                  key={name}
+                  variant="default"
+                  onClick={() => {
+                    copyClipboard(URL);
+                    setCopyText("copied");
+                    setTimeout(() => {
+                      setCopyText("copy");
+                    }, 1000);
+                  }}
+                >
                   {icon}
                   {link === "copy" ? (
-                    <p
-                      className="link-dim"
-                      onClick={() => {
-                        copyClipboard(URL);
-                        setCopyText("copied");
-                        setTimeout(() => {
-                          setCopyText("copy");
-                        }, 1000);
-                      }}
-                    >
-                      {name}
-                    </p>
+                    <p>{name}</p>
                   ) : (
-                    <At href={link} className="link-dim" external>
+                    <At href={link} external>
                       {name}
                     </At>
                   )}
-                </div>
+                </Button>
               );
             })}
           </div>
