42f209430390929c8c4499a668b1372b5599ebdc
Len E-Herng <leneherng@gmail.com>
2023-11-06 18:06:47 +0800

    chore: add routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -4,10 +4,12 @@ export const routes = {
   KATALOG_DN: "/katalog/dewan-negara",
   HANSARD_DR: "/hansard/dewan-rakyat",
   HANSARD_DN: "/hansard/dewan-negara",
+  KEHADIRAN_DR: "/kehadiran/dewan-rakyat",
+  KEHADIRAN_DN: "/kehadiran/dewan-negara",
 };
 
 export const static_routes: string[] = (() => {
-  let s_routes = Object.values(routes).filter(route => !["/katalog"].includes(route));
+  let s_routes = [""];//Object.values(routes).filter(route => !["/katalog"].includes(route));
 
   s_routes.forEach(route => {
     s_routes.push(`/en-GB${route}`);
