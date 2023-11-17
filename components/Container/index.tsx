ef768e59fb91468a5408d273b5778ec4dbc6692d
Len E-Herng <leneherng@gmail.com>
2023-11-17 21:34:02 +0800

    chore: remove divide

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -16,7 +16,7 @@ const Container: FunctionComponent<ContainerProps> = ({
     <div className={cn("flex h-full w-full justify-center", background)}>
       <div
         className={cn(
-          "md:px-4.5 divide-y divide-slate-200 dark:divide-zinc-800 h-full w-full max-w-screen-2xl px-3 lg:px-6",
+          "h-full w-full max-w-screen-2xl px-3 md:px-4.5 lg:px-6",
           className
         )}
       >
