048cca3cfa5fc057b59536e82388d986a0b04dec
Len E-Herng <leneherng@gmail.com>
2023-12-26 17:09:39 +0800

    feat: add Kamar Khas archive

--- lib/routes.ts
+++ lib/routes.ts
@@ -2,8 +2,10 @@ export const routes = {
   HOME: "/",
   KATALOG_DR: "/katalog/dewan-rakyat",
   KATALOG_DN: "/katalog/dewan-negara",
+  KATALOG_KK: "/katalog/kamar-khas",
   HANSARD_DR: "/hansard/dewan-rakyat",
   HANSARD_DN: "/hansard/dewan-negara",
+  HANSARD_KK: "/hansard/kamar-khas",
   KEHADIRAN_DR: "/kehadiran/dewan-rakyat",
   KEHADIRAN_DN: "/kehadiran/dewan-negara",
   SEJARAH_INDIVIDU: "/sejarah/individu",
@@ -13,7 +15,7 @@ export const routes = {
 };
 
 export const static_routes: string[] = (() => {
-  let s_routes = [routes.KATALOG_DR, routes.KATALOG_DN];
+  let s_routes = [routes.KATALOG_DR, routes.KATALOG_DN, routes.KATALOG_KK];
   
   s_routes.forEach(route => {
     s_routes.push(`/en-GB${route}`);
---
+++ pages/katalog/kamar-khas/[[...archive]].tsx
@@ -0,0 +1,67 @@
+import Metadata from "@components/Metadata";
+import CatalogueIndex from "@data-catalogue/index";
+import CatalogueIndexLayout from "@data-catalogue/layout";
+import { useTranslation } from "@hooks/useTranslation";
+import { get } from "@lib/api";
+import { AnalyticsProvider } from "@lib/contexts/analytics";
+import { withi18n } from "@lib/decorators";
+import { Page } from "@lib/types";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+
+const CatalogueIndexPage: Page = ({
+  meta,
+  archive,
+  params,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { t } = useTranslation("catalogue");
+
+  return (
+    <>
+      <Metadata
+        title={t("hero.header")}
+        description={t("hero.description")}
+        keywords={""}
+      />
+      <AnalyticsProvider meta={meta}>
+        <CatalogueIndexLayout>
+          <CatalogueIndex archive={archive} params={params} />
+        </CatalogueIndexLayout>
+      </AnalyticsProvider>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = withi18n(
+  ["catalogue", "enum"],
+  async ({ params }) => {
+    try {
+      const { data } = await get("api/catalogue/", {
+        house: "kamar-khas",
+      });
+
+      return {
+        props: {
+          meta: {
+            id: "catalogue-index-kk",
+            type: "misc",
+          },
+          archive: data.catalogue_list,
+          params: params,
+        },
+        revalidate: 86400, // 1 day
+      };
+    } catch (error) {
+      console.error(error);
+      return { notFound: true };
+    }
+  }
+);
+
+export default CatalogueIndexPage;
