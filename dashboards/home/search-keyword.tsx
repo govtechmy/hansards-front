8d2d9a4b5ac5a24c844311be3e7a47bec99afabe
Len E-Herng <leneherng@gmail.com>
2024-02-06 11:50:53 +0800

    fix: width, title i18n

--- dashboards/home/search-keyword.tsx
+++ dashboards/home/search-keyword.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "next-i18next";
 import Excerpts, { ExcerptsProps } from "./excerpts";
 import Keyword, { KeywordProps } from "./keyword";
-import { Container, Section, Skeleton } from "@components/index";
+import { Container, Skeleton } from "@components/index";
 import dynamic from "next/dynamic";
+import { capitalize } from "@lib/utils";
 
 /**
  * Search Keyword Dashboard
@@ -29,39 +31,56 @@ const SearchKeyword = ({
   timeseries,
   top_word_freq,
   top_speakers,
-}: SearchKeywordProps) => (
-  <Container className="divide-y divide-slate-200 dark:divide-zinc-800">
-    <Keyword query={query} timeseries={timeseries} />
-    {count > 0 && (
-      <>
-        <Excerpts count={count} excerpts={excerpts} query={query} />
-        {top_word_freq && (
-          <Section title="Related Words">
-            <BubbleCloud
-              className="w-full h-[350px] sm:h-[600px]"
-              data={Object.entries(top_word_freq).map(([word, freq]) => ({
-                id: word,
-                value: freq,
-              }))}
-            />
-          </Section>
-        )}
-        {top_speakers && (
-          <Section title="Top Speakers">
-            <BarMeter
-              layout="horizontal"
-              data={top_speakers.map((s) => ({
-                x: Object.keys(s)[0],
-                y: Object.values(s)[0],
-              }))}
-              relative
-              precision={0}
-            />
-          </Section>
-        )}
-      </>
-    )}
-  </Container>
-);
+}: SearchKeywordProps) => {
+  const { t } = useTranslation("home");
+
+  return (
+    <Container className="divide-y divide-border">
+      <Keyword query={query} timeseries={timeseries} />
+      {count > 0 && (
+        <>
+          <Excerpts count={count} excerpts={excerpts} query={query} />
+          {top_word_freq && (
+            <section className="py-8 lg:py-12 space-y-6 lg:space-y-8">
+              {/* Words related to {{ keyword }} */}
+              <h3 className="header text-center">
+                {t("words_related", { keyword: `"${query.q}"` })}
+              </h3>
+
+              <BubbleCloud
+                className="w-full h-[350px] sm:h-[900px]"
+                data={Object.entries(top_word_freq).map(([word, freq]) => ({
+                  id: word,
+                  value: freq,
+                }))}
+              />
+            </section>
+          )}
+          {top_speakers && (
+            <section className="py-8 lg:py-12 space-y-6 lg:space-y-8">
+              {/* "{{ keyword }}": Most spoken by */}
+              <h3 className="header text-center">
+                {t("most_spoken_by", {
+                  keyword: `"${capitalize(String(query.q))}"`,
+                })}
+              </h3>
+
+              <BarMeter
+                className="max-w-screen-sm mx-auto"
+                layout="horizontal"
+                data={top_speakers.map((s) => ({
+                  x: Object.keys(s)[0],
+                  y: Object.values(s)[0],
+                }))}
+                relative
+                precision={0}
+              />
+            </section>
+          )}
+        </>
+      )}
+    </Container>
+  );
+};
 
 export default SearchKeyword;
