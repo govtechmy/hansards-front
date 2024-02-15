613f5e1797c3287b5303b4f4a2f5a0b32a4f263a
Len E-Herng <leneherng@gmail.com>
2024-02-15 18:41:01 +0800

    chore: move getMatchText into this file

--- data-catalogue/hansard/search/highlight.tsx
+++ data-catalogue/hansard/search/highlight.tsx
@@ -1,15 +1,31 @@
-import { useContext } from "react";
-import { SearchContext } from "./context";
 import { COLOR } from "@lib/constants";
+import { useContext, useEffect, useMemo } from "react";
+import { SearchContext, SearchEventContext } from "./context";
+import { getMatchText } from "./match-text";
 
-type MatchData = string | Record<"slices" | "matches", string[]>;
+export function highlightKeyword(text: string, id: string) {
+  const { searchValue, activeId } = useContext(SearchContext);
+  const { onUpdateMatchList } = useContext(SearchEventContext);
 
-export function highlightKeyword(matchData: MatchData, id: string) {
-  if (typeof matchData === "string") return matchData;
+  const matchData = useMemo(
+    () => getMatchText(searchValue, text),
+    [searchValue, text]
+  );
+
+  useEffect(() => {
+    if (typeof matchData === "object") {
+      const matchIds = matchData.matches.map((_, i) => ({
+        id: `${id}_${i}`,
+        idCount: i,
+      }));
+      onUpdateMatchList(matchIds);
+    } else onUpdateMatchList([]);
+  }, [matchData]);
+
+  if (typeof matchData === "string") return <>{text}</>;
   else {
-    const { activeId } = useContext(SearchContext);
     return (
-      <p>
+      <>
         {matchData.slices.map((speech, i) => {
           const matchId = `${id}_${i}`;
           const isHighlighted = matchId === activeId;
@@ -34,7 +50,7 @@ export function highlightKeyword(matchData: MatchData, id: string) {
             speech
           );
         })}
-      </p>
+      </>
     );
   }
 }
