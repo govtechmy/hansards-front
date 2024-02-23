390a11bdb3a53ddf8b08dff0d0b17b2efacd9598
Len E-Herng <leneherng@gmail.com>
2024-02-23 18:33:59 +0800

    refactor: use floating-ui

--- components/Sheet/index.tsx
+++ components/Sheet/index.tsx
@@ -1,130 +1,257 @@
-import * as React from "react";
-import * as SheetPrimitive from "@radix-ui/react-dialog";
-
+import Button from "@components/Button";
+import {
+  useFloating,
+  useClick,
+  useDismiss,
+  useRole,
+  useInteractions,
+  useMergeRefs,
+  FloatingPortal,
+  FloatingFocusManager,
+  FloatingOverlay,
+  useId,
+} from "@floating-ui/react";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import { body, header } from "@lib/configs/font";
 import { cn } from "@lib/helpers";
+import {
+  ButtonHTMLAttributes,
+  Dispatch,
+  HTMLProps,
+  ReactNode,
+  SetStateAction,
+  cloneElement,
+  createContext,
+  forwardRef,
+  isValidElement,
+  useContext,
+  useLayoutEffect,
+  useMemo,
+  useState,
+} from "react";
 
-const Sheet = SheetPrimitive.Root;
-
-const SheetTrigger = SheetPrimitive.Trigger;
-
-const SheetClose = SheetPrimitive.Close;
-
-const SheetPortal = SheetPrimitive.Portal;
-
-const SheetOverlay = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Overlay
-    className={cn(
-      "fixed inset-0 z-50 bg-[#000]/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-    ref={ref}
-  />
-));
-SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
-
-interface SheetContentProps
-  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content> {
-  side?: "top" | "bottom" | "left" | "right";
+interface DialogOptions {
+  initialOpen?: boolean;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
 }
 
-const SheetContent = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Content>,
-  SheetContentProps
->(({ side = "left", className, children, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
-    <SheetPrimitive.Content
+export function useDialog({
+  initialOpen = false,
+  open: controlledOpen,
+  onOpenChange: setControlledOpen,
+}: DialogOptions = {}) {
+  const [uncontrolledOpen, setUncontrolledOpen] = useState(initialOpen);
+  const [labelId, setLabelId] = useState<string | undefined>();
+  const [descriptionId, setDescriptionId] = useState<string | undefined>();
+
+  const open = controlledOpen ?? uncontrolledOpen;
+  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+
+  const data = useFloating({
+    open,
+    onOpenChange: setOpen,
+  });
+
+  const context = data.context;
+
+  const click = useClick(context, {
+    enabled: controlledOpen == null,
+  });
+  const dismiss = useDismiss(context, { outsidePressEvent: "mousedown" });
+  const role = useRole(context);
+
+  const interactions = useInteractions([click, dismiss, role]);
+
+  return useMemo(
+    () => ({
+      open,
+      setOpen,
+      ...interactions,
+      ...data,
+      labelId,
+      descriptionId,
+      setLabelId,
+      setDescriptionId,
+    }),
+    [open, setOpen, interactions, data, labelId, descriptionId]
+  );
+}
+
+type ContextType =
+  | (ReturnType<typeof useDialog> & {
+    setLabelId: Dispatch<React.SetStateAction<string | undefined>>;
+    setDescriptionId: Dispatch<SetStateAction<string | undefined>>;
+  })
+  | null;
+
+const DialogContext = createContext<ContextType>(null);
+
+export const useDialogContext = () => {
+  const context = useContext(DialogContext);
+
+  if (context == null) {
+    throw new Error("Dialog components must be wrapped in <Dialog />");
+  }
+
+  return context;
+};
+
+export function Sheet({
+  children,
+  ...options
+}: {
+  children: ReactNode;
+} & DialogOptions) {
+  const dialog = useDialog(options);
+  return (
+    <DialogContext.Provider value={dialog}>{children}</DialogContext.Provider>
+  );
+}
+
+interface SheetTriggerProps {
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export const SheetTrigger = forwardRef<
+  HTMLElement,
+  HTMLProps<HTMLElement> & SheetTriggerProps
+>(function SheetTrigger({ children, asChild = false, ...props }, propRef) {
+  const context = useDialogContext();
+  const childrenRef = (children as any).ref;
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+
+  // `asChild` allows the user to pass any element as the anchor
+  if (asChild && isValidElement(children)) {
+    return cloneElement(
+      children,
+      context.getReferenceProps({
+        ref,
+        ...props,
+        ...children.props,
+        "data-state": context.open ? "open" : "closed",
+      })
+    );
+  }
+
+  return (
+    <button
       ref={ref}
-      className={cn(
-        "fixed z-50 gap-4 bg-background border-border p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
-        {
-          "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top":
-            side === "top",
-          "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom":
-            side === "bottom",
-          "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm":
-            side === "left",
-          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm":
-            side === "right",
-        },
-        header.variable,
-        body.variable,
-        "font-body",
-        className
-      )}
-      {...props}
+      // The user can style the trigger based on the state
+      data-state={context.open ? "open" : "closed"}
+      {...context.getReferenceProps(props)}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
-        <XMarkIcon className="h-5 w-5" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
-    </SheetPrimitive.Content>
-  </SheetPortal>
-));
-SheetContent.displayName = SheetPrimitive.Content.displayName;
+    </button>
+  );
+});
 
-const SheetHeader = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("flex flex-col space-y-2", className)} {...props} />
-);
-SheetHeader.displayName = "SheetHeader";
+export const SheetContent = forwardRef<
+  HTMLDivElement,
+  HTMLProps<HTMLDivElement> & { side?: "top" | "bottom" | "left" | "right" }
+>(function SheetContent({ side = "left", className, children, ...props }, propRef) {
+  const { context: floatingContext, ...context } = useDialogContext();
+  const ref = useMergeRefs([context.refs.setFloating, propRef]);
 
-const SheetFooter = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
-    {...props}
-  />
-);
-SheetFooter.displayName = "SheetFooter";
+  if (!floatingContext.open) return null;
 
-const SheetTitle = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Title
-    ref={ref}
-    className={cn("text-lg font-semibold text-foreground", className)}
-    {...props}
-  />
-));
-SheetTitle.displayName = SheetPrimitive.Title.displayName;
+  return (
+    <FloatingPortal>
+      <FloatingOverlay className="fixed inset-0 bg-[#000]/80 z-50" lockScroll>
+        <FloatingFocusManager context={floatingContext}>
+          <div
+            ref={ref}
+            className={cn(
+              "fixed z-50 gap-4 bg-background border-border p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+              {
+                "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top":
+                  side === "top",
+                "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom":
+                  side === "bottom",
+                "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm":
+                  side === "left",
+                "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm":
+                  side === "right",
+              },
+              header.variable, body.variable, "font-body",
+              className
+            )}
+            data-state={context.open ? 'open' : 'closed'}
+            {...props}
+          >
+            {children}
+            <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
+              <XMarkIcon className="h-5 w-5" />
+              <span className="sr-only">Close</span>
+            </SheetClose>
+          </div>
+        </FloatingFocusManager>
+      </FloatingOverlay>
+    </FloatingPortal>
+  );
+});
 
-const SheetDescription = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Description
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-));
-SheetDescription.displayName = SheetPrimitive.Description.displayName;
+export const SheetHeading = forwardRef<
+  HTMLHeadingElement,
+  HTMLProps<HTMLHeadingElement>
+>(function SheetHeading({ children, ...props }, ref) {
+  const { setLabelId } = useDialogContext();
+  const id = useId();
 
-export {
-  Sheet,
-  SheetPortal,
-  SheetOverlay,
-  SheetTrigger,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetFooter,
-  SheetTitle,
-  SheetDescription,
-};
+  // Only sets `aria-labelledby` on the  root element
+  // if this component is mounted inside it.
+  useLayoutEffect(() => {
+    setLabelId(id);
+    return () => setLabelId(undefined);
+  }, [id, setLabelId]);
+
+  return (
+    <div {...props} ref={ref} id={id}>
+      {children}
+    </div>
+  );
+});
+
+export const SheetDescription = forwardRef<
+  HTMLParagraphElement,
+  HTMLProps<HTMLParagraphElement>
+>(function SheetDescription({ children, ...props }, ref) {
+  const { setDescriptionId } = useDialogContext();
+  const id = useId();
+
+  // Only sets `aria-describedby` on the Sheet root element
+  // if this component is mounted inside it.
+  useLayoutEffect(() => {
+    setDescriptionId(id);
+    return () => setDescriptionId(undefined);
+  }, [id, setDescriptionId]);
+
+  return (
+    <div {...props} ref={ref} id={id}>
+      {children}
+    </div>
+  );
+});
+
+export const SheetClose = forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement>
+>(function SheetClose(props, ref) {
+  const { setOpen } = useDialogContext();
+  const { className, ...rest } = props
+  return (
+    <Button
+      ref={ref}
+      variant="ghost"
+      className={cn(
+        "hover:bg-slate-100 dark:hover:bg-zinc-800 group rounded-full p-2 -m-2",
+        className
+      )}
+      onClick={() => setOpen(false)}
+      {...rest}
+    >
+      <XMarkIcon className="text-zinc-500 h-5 w-5 group-hover:text-zinc-900 dark:group-hover:text-white" />
+    </Button>
+  );
+});
