551b1677c8850f3dca64b96547b88ef2d7b9be52
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2025-03-21 11:13:32 +0800

    chore: 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,25 +1,44 @@
-import Container from "@components/Container";
-import ErrorStatus from "@components/ErrorStatus";
 import Metadata from "@components/Metadata";
-import { withi18n } from "@lib/decorators";
+import { Button, ButtonIcon } from "@govtechmy/myds-react/button";
+import { ArrowBackIcon } from "@govtechmy/myds-react/icon";
+import { Link } from "@govtechmy/myds-react/link";
 import { useTranslation } from "@hooks/useTranslation";
+import { withi18n } from "@lib/decorators";
 import { Page } from "@lib/types";
 import { GetStaticProps } from "next";
 
 const Error404: Page = () => {
   const { t } = useTranslation("error");
+
   return (
     <>
       <Metadata title={t("404.title") as string} keywords={""} />
 
-      <Container className="min-h-[76vh] pt-7 text-zinc-900">
-        <ErrorStatus
-          title={t("404.title") as string}
-          description={t("404.description")}
-          code={404}
-          reason={t("404.reason")}
-        />
-      </Container>
+      <main className="flex min-h-[90vh] flex-col space-y-6 pt-40 text-center">
+        <p className="font-poppins text-[100px] leading-[100px] text-bg-black-400">
+          404
+        </p>
+        <div className="space-y-4">
+          <h2 className="font-poppins text-heading-xs font-semibold md:text-heading-sm">
+            Halaman tidak dijumpai
+          </h2>
+          <div>
+            {["Page not found", "页面未找到", "பக்கம் காணப்படவில்லை"].map(
+              error => (
+                <p className="text-lg text-txt-black-700">{error}</p>
+              )
+            )}
+          </div>
+        </div>
+        <Button variant="default-outline" size="small" asChild>
+          <Link href="/" underline="none" className="mx-auto mt-10">
+            <ButtonIcon>
+              <ArrowBackIcon />
+            </ButtonIcon>
+            Kembali ke Halaman Utama
+          </Link>
+        </Button>
+      </main>
     </>
   );
 };
