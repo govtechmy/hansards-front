27fbf73f163236d498d47e6e6a7898c459710ccd
harrisazmi <azmi.harris@gmail.com>
2026-03-03 11:24:56 +0800

    hotfix: update label in search mp by duplicating

--- dashboards/home/filter-options.tsx
+++ dashboards/home/filter-options.tsx
@@ -1,11 +1,15 @@
 export const DEWANS = ["dewan-negara", "dewan-rakyat", "kamar-khas"];
 
 export const ALL_PARTIES = "all_parties";
+export const ALL_PARTIES2 = "all_parties2";
 
 export const ALL_AGES = "all_ages";
+export const ALL_AGES2 = "all_ages2";
 export const AGES = ["18-29", "30-39", "40-49", "50-59", "60-69", "70"];
 
 export const BOTH_GENDERS = "both_genders";
+export const BOTH_GENDERS2 = "both_genders2";
+
 export const GENDERS = [BOTH_GENDERS, "m", "f"];
 
 export const ALL_ETHNICITIES = "all_ethnicities";
